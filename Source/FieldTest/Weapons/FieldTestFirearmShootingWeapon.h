84c9480d6e3dc8b98cf38abc63872c4a1161bc99
YitongHuGpm20 <43768506+YitongHuGpm20@users.noreply.github.com>
2026-09-23 21:16:44 -0700

    Moved properties to shooting weapon class

--- Source/FieldTest/Weapons/FieldTestFirearmShootingWeapon.h
+++ Source/FieldTest/Weapons/FieldTestFirearmShootingWeapon.h
@@ -15,18 +15,6 @@ public:
 	AFieldTestFirearmShootingWeapon();
 
 protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon|Firearm|Ammo", meta = (ToolTip = "Maximum number of rounds the magazine can hold."))
-	int32 MagazineSize = 0;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Firearm|Ammo", meta = (ToolTip = "Number of rounds currently loaded in the magazine."))
-	int32 CurrentAmmo = 0;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Firearm|Ammo", meta = (ToolTip = "Total number of reserve rounds. Divide by Magazine Size to determine the number of full spare magazines."))
-	int32 ReserveAmmo = 0;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon|Firearm", meta = (ToolTip = "Weapon fire rate in rounds fired per minute."))
-	float FireRate = 0.0f;
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon|Firearm")
 	USceneComponent* LeftHandGrip;
 
--- Source/FieldTest/Weapons/FieldTestShootingWeapon.h
+++ Source/FieldTest/Weapons/FieldTestShootingWeapon.h
@@ -7,4 +7,17 @@ UCLASS(Blueprintable)
 class FIELDTEST_API AFieldTestShootingWeapon : public AFieldTestWeapon
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon|Shooting|Ammo", meta = (ToolTip = "Maximum number of rounds the magazine can hold."))
+	int32 MagazineSize = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Shooting|Ammo", meta = (ToolTip = "Number of rounds currently loaded in the magazine."))
+	int32 CurrentAmmo = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon|Shooting|Ammo", meta = (ToolTip = "Total number of reserve rounds. Divide by Magazine Size to determine the number of full spare magazines."))
+	int32 ReserveAmmo = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon|Shooting", meta = (ToolTip = "Weapon fire rate in rounds fired per minute."))
+	float FireRate = 0.0f;
 };
